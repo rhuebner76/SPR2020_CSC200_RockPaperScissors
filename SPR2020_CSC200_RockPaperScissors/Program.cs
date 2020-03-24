adbf14ccb56e655fd3d88aeddaef621bd0779f17
Russell Huebner <russell.j.huebner@outlook.com>
2020-03-24 10:52:42 -0700

    Added GameManager and Hand classes, refactored to remove procedural branching from main

--- SPR2020_CSC200_RockPaperScissors/Program.cs
+++ SPR2020_CSC200_RockPaperScissors/Program.cs
@@ -6,90 +6,196 @@ using System.Threading.Tasks;
 
 namespace CSC200_RockPaperScissors
 {
+    class Player
+    {
+        // 
+    }
+
+    class Hand
+    {
+        // data fields
+        private readonly string _value;
+
+        // constructor
+        public Hand(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return this._value; }
+        }
+
+        public int CompareTo(Hand other)
+        {
+            // returns -1 when this hand is less then other
+            // returns 0 when this hand is equal to other
+            // return 1 when this hand is great then other
+
+            if (this.Value == other.Value)
+                return 0;
+
+            if (this.Value == "rock")
+            {
+                if (other.Value == "scissors")
+                {
+                    return 1;
+                }
+                else if (other.Value == "paper")
+                {
+                    return -1;
+                }
+            }
+
+            if (this.Value == "paper")
+            {
+                if (other.Value == "rock")
+                {
+                    return 1;
+                }
+                else if (other.Value == "scissors")
+                {
+                    return -1;
+                }
+            }
+
+            if (this.Value == "scissors")
+            {
+                if (other.Value == "paper")
+                {
+                    return 1;
+                }
+                else if (other.Value == "rock")
+                {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    class GameManager
+    {
+        // data fields
+
+        // constructor
+        public GameManager()
+        {
+
+        }
+
+        private Hand GetUserHand()
+        {
+            bool valid = false;
+            Hand result = new Hand("");
+
+            while (valid == false)
+            {
+                Console.WriteLine("Choose R:Rock, P:Paper, or S:Scissors (Q to Quit).");
+                string userChoice = Console.ReadLine();
+
+                // user could enter an empty line
+                if (string.IsNullOrEmpty(userChoice))
+                    continue;
+
+                // user could enter an invalid value
+                switch (userChoice.ToUpper())
+                {
+                    case "ROCK":
+                    case "R":
+                        result = new Hand("rock");
+                        valid = true;
+                        break;
+                    case "PAPER":
+                    case "P":
+                        result = new Hand("paper");
+                        valid = true;
+                        break;
+                    case "SCISSORS":
+                    case "S":
+                        result = new Hand("scissors");
+                        valid = true;
+                        break;
+                    case "QUIT":
+                    case "Q":
+                        result = new Hand("quit");
+                        valid = true;
+                        break;
+                }
+
+                Console.Clear();
+            }
+
+            return result;
+        }
+
+        private Hand GenerateRandomHand()
+        {
+            // 1 = rock, 2 = paper, 3 = scissors
+            Random random = new Random();
+            int value = random.Next(1, 3);
+            Hand result;
+
+            if (value == 1)
+            {
+                result = new Hand("rock");
+            }
+            else if (value == 2)
+            {
+                result = new Hand("paper");
+            }
+            else if (value == 3)
+            {
+                result = new Hand("scissors");
+            }
+            else
+            {
+                result = new Hand(string.Empty);
+            }
+
+            return result;
+        }
+
+        // methods
+        public void StartPlay()
+        {
+            // get the hand from the user
+            Hand userHand = GetUserHand();
+            while (userHand.Value != "quit")
+            {
+                // get a hand for the computer
+                Hand computerHand = GenerateRandomHand();
+
+                if (computerHand.CompareTo(userHand) < 0)
+                {
+                    Console.WriteLine("The player chose {0} and computer chose {1}, player wins!", userHand.Value, computerHand.Value);
+                }
+                else if (computerHand.CompareTo(userHand) > 0)
+                {
+                    Console.WriteLine("The player chose {0} and computer chose {1}, player loses!", userHand.Value, computerHand.Value);
+                }
+                else
+                {
+                    Console.WriteLine("The computer and you chose {0}, its a tie!", computerHand.Value);
+                }
+
+                Console.WriteLine();
+
+                // get the hand from the user
+                userHand = GetUserHand();
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Do you choose rock, paper, or scissors.");
-            string userChoice = Console.ReadLine();
+            GameManager manager = new GameManager();
 
-            Random r = new Random();
-            int computerChoice = r.Next(4);
-
-            if (computerChoice == 1)
-            {
-                if (userChoice == "rock")
-                {
-                    Console.WriteLine("The computer chose rock");
-                    Console.WriteLine("It is a tie ");
-                }
-                else if (userChoice == "paper")
-                {
-                    Console.WriteLine("The computer chose paper");
-                    Console.WriteLine("It is a tie ");
-
-                }
-                else if (userChoice == "scissors")
-                {
-                    Console.WriteLine("The computer chose scissors");
-                    Console.WriteLine("It is a tie ");
-                }
-                else
-                {
-                    Console.WriteLine("You must choose rock, paper, or scissors!");
-
-                }
-
-            }
-            else if (computerChoice == 2)
-            {
-                if (userChoice == "rock")
-                {
-                    Console.WriteLine("The computer chose paper");
-                    Console.WriteLine("Sorry you lose, paper beat rock");
-
-                }
-                else if (userChoice == "paper")
-                {
-                    Console.WriteLine("The computer chose scissors");
-                    Console.WriteLine("Sorry you lose, scissors beat paper ");
-
-                }
-                else if (userChoice == "scissors")
-                {
-                    Console.WriteLine("The computer chose rock");
-                    Console.WriteLine("Sorry you lose, rock beats scissors");
-                }
-                else
-                {
-                    Console.WriteLine("You must choose rock, paper or scissors!");
-                }
-            }
-            else if (computerChoice == 3)
-            {
-                if (userChoice == "rock")
-                {
-                    Console.WriteLine("The computer chose scissors");
-                    Console.WriteLine("You win, rock beats scissors");
-
-                }
-                else if (userChoice == "paper")
-                {
-                    Console.WriteLine("The computer chose rock");
-                    Console.WriteLine("You win, paper beats rock");
-
-                }
-                else if (userChoice == "scissors")
-                {
-                    Console.WriteLine("The computer chose paper");
-                    Console.WriteLine("You win, scissors beat paper");
-
-                }
-                else
-                {
-                    Console.WriteLine("You must choose rock, paper or scissors!");
-                }
-            }
+            manager.StartPlay();
 
             Console.ReadLine();
         }
