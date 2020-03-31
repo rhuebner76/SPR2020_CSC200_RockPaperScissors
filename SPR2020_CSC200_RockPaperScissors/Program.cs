f5a4a624aa84465d54a66858141521f5b9060d30
Russell Huebner <russell.j.huebner@outlook.com>
2020-03-31 09:58:54 -0700

    Added spock and lizard

--- SPR2020_CSC200_RockPaperScissors/Program.cs
+++ SPR2020_CSC200_RockPaperScissors/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace CSC200_RockPaperScissors
 {
+    // SOLID
+    // S - SRP - Single Responsibility Principle 
+    //      An object should have only one purpose
+    // O - OCP - Open Closed Principle
+    //      And object should be open for extension, closed for modification
+    // L - Liskov Substitution Principle
     class Player
     {
         // 
@@ -29,9 +35,9 @@ namespace CSC200_RockPaperScissors
 
         public int CompareTo(Hand other)
         {
-            // returns -1 when this hand is less then other
-            // returns 0 when this hand is equal to other
-            // return 1 when this hand is great then other
+            // returns -1 when this hand is less then other (this hand loses)
+            // returns 0 when this hand is equal to other   (this hand ties)
+            // return 1 when this hand is great then other  (this hand wins)
 
             if (this.Value == other.Value)
                 return 0;
@@ -40,11 +46,19 @@ namespace CSC200_RockPaperScissors
             {
                 if (other.Value == "scissors")
                 {
-                    return 1;
+                    return 1; // rock crushes scissors
+                }
+                else if (other.Value == "spock")
+                {
+                    return -1; // spock vaporizes rock
+                }
+                else if (other.Value == "lizard")
+                {
+                    return 1; // rock crushes lizard
                 }
                 else if (other.Value == "paper")
                 {
-                    return -1;
+                    return -1; // paper covers rock
                 }
             }
 
@@ -52,11 +66,19 @@ namespace CSC200_RockPaperScissors
             {
                 if (other.Value == "rock")
                 {
-                    return 1;
+                    return 1; // paper covers rock
+                }
+                else if (other.Value == "spock")
+                {
+                    return 1; // paper disproves spock
+                }
+                else if (other.Value == "lizard")
+                {
+                    return -1; // paper gets ate by lizard
                 }
                 else if (other.Value == "scissors")
                 {
-                    return -1;
+                    return -1; // paper gets cut scissors
                 }
             }
 
@@ -66,9 +88,57 @@ namespace CSC200_RockPaperScissors
                 {
                     return 1;
                 }
+                else if (other.Value == "spock")
+                {
+                    return -1; // scissors smashed by spock
+                }
+                else if (other.Value == "lizard")
+                {
+                    return 1; // scissors decapitates lizard
+                }
                 else if (other.Value == "rock")
                 {
+                    return -1; // 
+                }
+            }
+
+            if (this.Value == "spock")
+            {
+                if (other.Value == "paper")
+                {
                     return -1;
+                }
+                else if (other.Value == "scissors")
+                {
+                    return 1;
+                }
+                else if (other.Value == "lizard")
+                {
+                    return -1;
+                }
+                else if (other.Value == "rock")
+                {
+                    return 1; // 
+                }
+            }
+
+            if (this.Value == "lizard")
+            {
+                if (other.Value == "paper")
+                {
+                    return 1;
+                }
+                else if (other.Value == "scissors")
+                {
+                    return -1;
+                }
+                else if (other.Value == "spock")
+                {
+                    return 1;
+                }
+                else if (other.Value == "rock")
+                {
+                    return -1; // 
                 }
             }
 
@@ -93,7 +163,7 @@ namespace CSC200_RockPaperScissors
 
             while (valid == false)
             {
-                Console.WriteLine("Choose R:Rock, P:Paper, or S:Scissors (Q to Quit).");
+                Console.WriteLine("Choose R:Rock, P:Paper, S:Scissors, V:Spock, or L:Lizard (Q to Quit).");
                 string userChoice = Console.ReadLine();
 
                 // user could enter an empty line
@@ -118,6 +188,16 @@ namespace CSC200_RockPaperScissors
                         result = new Hand("scissors");
                         valid = true;
                         break;
+                    case "SPOCK":
+                    case "V":
+                        result = new Hand("spock");
+                        valid = true;
+                        break;
+                    case "LIZARD":
+                    case "L":
+                        result = new Hand("lizard");
+                        valid = true;
+                        break;
                     case "QUIT":
                     case "Q":
                         result = new Hand("quit");
@@ -133,9 +213,9 @@ namespace CSC200_RockPaperScissors
 
         private Hand GenerateRandomHand()
         {
-            // 1 = rock, 2 = paper, 3 = scissors
+            // 1 = rock, 2 = paper, 3 = scissors, 4 = spock, 5 = lizard
             Random random = new Random();
-            int value = random.Next(1, 3);
+            int value = random.Next(1, 5);
             Hand result;
 
             if (value == 1)
@@ -149,6 +229,14 @@ namespace CSC200_RockPaperScissors
             else if (value == 3)
             {
                 result = new Hand("scissors");
+            }
+            else if (value == 4)
+            {
+                result = new Hand("spock");
+            }
+            else if (value == 5)
+            {
+                result = new Hand("lizard");
             }
             else
             {
